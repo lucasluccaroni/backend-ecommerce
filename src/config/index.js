33ac2ea686c0aa272e9c92fe7b7151634a33cccd
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2024-08-10 13:47:10 -0300

    empezando montaje en railway. Cambiando variables de entorno, script de inicio, seteando MongoAtlas, probandoq que funcione

--- src/config/index.js
+++ src/config/index.js
@@ -3,9 +3,9 @@ const { logger } = require("../logger/logger")
 config()
 
 // Base de datos dev o prod
-const dbName = process.env.NODE_ENV === "prod" ? "coderhouse-backend-capas" : "base-dev"
-process.env.DB_NAME = dbName
-// logger.debug(` DTABASE NAME => ${process.env.DB_NAME}`)
+// const dbName = process.env.NODE_ENV === "prod" ? "coderhouse-backend-capas" : "base-dev"
+// process.env.DB_NAME = dbName
+logger.debug(` DTABASE NAME => ${process.env.DB_NAME}`)
 
 module.exports = {
     mongoUri: process.env.MONGO_URI,
